ad2881a635aec293be56ecad825c7d7d47a88b8d
Ben Newman <ben@meteor.com>
2015-02-19 17:46:46 -0500

    Placeholder for editor/locker icon.

--- client/helpers.js
+++ client/helpers.js
@@ -2,8 +2,22 @@ Template.node.helpers({
   indent: function () {
     return 20;
   },
+
   contextMenu: function () {
     return this._id === Session.get("contextMenuNodeId");
+  },
+
+  getEditorDeindent: function() {
+    var node = this;
+    var root = Nodes.findOne(Session.get("rootNodeId"));
+    var depth = 0;
+
+    while (node && root && node._id !== root._id) {
+      node = node.getParent();
+      ++depth;
+    }
+
+    return -48 * depth;
   }
 });
 
