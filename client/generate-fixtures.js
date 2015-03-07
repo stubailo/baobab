a4ca753418cd181b59bf12d9c20b17287122b179
Sashko Stubailo <sashko@stubailo.com>
2015-03-07 14:23:06 -0800

    Remove some dead code

--- client/generate-fixtures.js
+++
@@ -1,7 +0,0 @@
-generateFixtures = function () {
-  var rootID = Meteor.user().rootNodeId;
-  var welcomeID = Nodes.insertNode("Welcome to Baobab!", rootID);
-  var typingID = Nodes.insertNode("Start typing below", welcomeID);
-  Nodes.insertNode("", typingID);
-  Nodes.insertNode("like so", typingID);
-};
--- packages/nodes/node-publish.js
+++ packages/nodes/node-publish.js
@@ -9,17 +9,6 @@ if (Meteor.isServer) {
       });
     }
   });
-
-  Meteor.publish('nodesTokenAuth', function (token) {
-    if (token) {
-      return Nodes.find({
-        $or: [
-          {"permissions.readWrite.id": token},
-          {"permissions.readOnly.id": token}
-        ]
-      });
-    }
-  });
 }
 
 if (Meteor.isClient) {
